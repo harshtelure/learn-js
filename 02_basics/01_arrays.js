e09055791d1c9ca7ce26c83dc8db890d8c54b26f
harshtelure <telureharsh@gmail.com>
2025-07-03 12:19:32 +0000

    3/7 post luch arrays complete

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,46 @@
+// const myArr = ["Harsh", 331063, "Telure", true]
+// console.log(myArr);
+
+// const myArr1 = [0,12,2,6,5,4]
+// const myArr2 = myArr1
+// myArr2.pop()
+// console.log(myArr1);
+
+// const myArr1 = [0,12,2,6,5,4]
+// const myArr2 = new Array(0,12,2,6,5,4)
+
+// myArr1.pop()
+// console.log(myArr1);
+// console.log(myArr2);
+
+// const myArr1 = [0,12,2,6,5,4]
+// myArr1.push(10)
+// console.log(myArr1);
+// console.log(myArr1.pop());
+// console.log(myArr1);
+// myArr1.unshift(10)
+// console.log(myArr1);
+// console.log(myArr1.shift());
+// console.log(myArr1);
+
+const myArr1 = [0,12,2,6,5,4]
+console.log(myArr1);
+
+const myArr2 = myArr1.slice(1,3)
+console.log(myArr2);
+console.log(myArr1);
+
+const myArr3 = myArr1.splice(1,3)
+console.log(myArr3);
+console.log(myArr1);
+
+const myarr4 = myArr1.join();
+console.log(myarr4);
+console.log(myArr1);
+
+
+
+
+
+
+
---
+++ 02_basics/02_arrrays.js
@@ -0,0 +1,31 @@
+// const myArr1 = [0,12,2,6,5,4]
+// console.log(myArr1);
+
+// const myArr2 = [10,132,24,64,59,14]
+// console.log(myArr2);
+
+// myArr1.push(myArr2)
+// console.log(myArr1);
+
+// const myArr3 = myArr1.concat(myArr2)
+// console.log(myArr3);
+
+// const myArr3 = [...myArr1, ...myArr2]
+// console.log(myArr3);
+
+// const myArr1 = [0,12,[1,5,[4,[1,2],4,5],6,4],2,6,[1,2],5,4]
+// console.log(myArr1);
+
+// const myArr2 = myArr1.flat(Infinity);
+// console.log(myArr2);
+
+// const myArr1 = Array.isArray("Harsh")
+// const myArr = Array.from("Harsh")
+// console.log(myArr);
+// console.log(myArr1);
+
+const score3 = 100
+const score2 = 2000
+const score1 = 300
+const myArr = Array.of(score1,score2,score3)
+console.log(myArr);
