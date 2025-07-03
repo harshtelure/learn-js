66e82c4478595af9db6bb5b1f2e7af4215578b92
harshtelure <telureharsh@gmail.com>
2025-07-03 05:31:49 +0000

    3/7 pre lunch variables and datatypes learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 331063
+let accountEmail = "teluerharsh@gmail.com"
+var accountPassword = "it's7584hate"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 63 // not allowed
+
+
+accountEmail = "harstelure@gmail.com"
+accountPassword = "harsh1289"
+accountCity = "Nashik"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //traet js code as newer version of javascript
+
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+let id=63
+let name="harsh"
+let student = true
+let job
+let salary=null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof id)
+console.log(typeof name)
+console.log(typeof job)
+console.log(typeof salary);
+console.log(typeof student);
+
